da66a6ef9fd5b52c5d5db1132e0cd9b5bb34f16f
Israel Tavares <israelbulcaotavares@gmail.com>
2021-11-09 12:29:02 -0300

    Obtendo a data das últimas transações

---
+++ src/components/Forms/CategorySelectButton/styles.ts
@@ -0,0 +1,31 @@
+import  styled from 'styled-components/native'
+import { RectButton } from 'react-native-gesture-handler'
+import { Feather } from '@expo/vector-icons' 
+import { RFValue } from 'react-native-responsive-fontsize';
+ 
+
+export const Container = styled(RectButton).attrs({
+    activeOpacity: 0.7
+})`
+   
+  background-color: ${({ theme }) => theme.colors.shape};
+  flex-direction: row;
+ justify-content: space-between;
+  align-items: center;
+
+  padding: ${RFValue(18)}px;
+  border-radius: 5px;
+  
+  
+`
+
+export const Category = styled.Text`
+    font-family: ${({ theme}) => theme.fonts.regular};
+    font-size: ${RFValue(14)}px; 
+`;
+
+export const Icon = styled(Feather)`
+    font-size: ${RFValue(20)}px;
+    color:${({ theme}) => theme.colors.text}
+`;
+
