db0a3fc939bdf2f5dda7ae1e4661b856ae0f9225
Israel Tavares <israelbulcaotavares@gmail.com>
2021-11-11 22:49:08 -0300

    Botão de Login Social

--- src/screens/SignSign/index.tsx
+++ src/screens/SignSign/index.tsx
@@ -5,18 +5,21 @@ import AppleSvg from '../../assets/apple.svg'
 import GoogleSvg from '../../assets/google.svg'
 import LogoSvg from '../../assets/logo.svg'
 
-import { 
-Container,
-Header,
-TitleWrapper,
-Title,
-SignInTitle,
-Footer,
+import { SignInSocialButton } from '../../components/SignInSocialButton';
+
+import {
+    Container,
+    Header,
+    TitleWrapper,
+    Title,
+    SignInTitle,
+    Footer,
+    FooterWrapper
 } from './styles'
 
-export function SignIn(){
+export function SignIn() {
 
-    return(
+    return (
         <Container>
             <Header>
                 <TitleWrapper>
@@ -24,24 +27,37 @@ export function SignIn(){
                         width={RFValue(120)}
                         height={RFValue(68)}
                     />
-                    
-                <Title>
-                    Controle suas{'\n'} 
-                    finanças de forma{'\n'}  
-                    muito simples{'\n'} 
-                </Title>
 
-                <SignInTitle>
-                    Faça login com {'\n'} 
-                    uma das conta abaixo
-                </SignInTitle>
- 
+                    <Title>
+                        Controle suas{'\n'}
+                        finanças de forma{'\n'}
+                        muito simples{'\n'}
+                    </Title>
+
+                    <SignInTitle>
+                        Faça login com {'\n'}
+                        uma das conta abaixo
+                    </SignInTitle>
+
                 </TitleWrapper>
 
             </Header>
 
             <Footer>
+                <FooterWrapper>
+                    <SignInSocialButton
+                        title="Entrar com Google"
+                        svg={GoogleSvg}
 
+                    />
+
+                    <SignInSocialButton
+                        title="Entrar com Apple"
+                        svg={AppleSvg}
+
+                    />
+
+                </FooterWrapper>
             </Footer>
         </Container>
 
